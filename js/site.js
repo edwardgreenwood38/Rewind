d79aa4f16867717bef77facfd8037917599b34cb
Edward E Greenwood <edwardgreenwood38@gmail.com>
2021-09-05 18:21:15 -0400

    code desc added
    added description to code page. add git link to app on all pages

--- js/site.js
+++ js/site.js
@@ -1,4 +1,36 @@
-// call helloworld
-function HelloWorld() {
-    alert("hello world");
+// get string that user entered
+// controller
+function getString() {
+    // always hide to start
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+// reverse the string
+//logic
+function reverseString(userString) {
+    let revString = [];
+
+    // reverse a string using a for loop
+    for (let i = userString.length - 1; i >= 0; i--)
+    {
+        revString += userString[i];
+    }
+
+    return revString;
+}
+
+// dsiplay the reversed string
+// view 
+function displayString(revString) {
+    // write message to page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    // show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
